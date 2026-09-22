76d21278be316e31f172128fa678f798981910c7
4wk7y6d2bw-cloud <4wk7y6d2bw@privaterelay.appleid.com>
2026-09-22 05:16:24 +0200

    Set starting cash to 500 dollars

--- base/auth.php
+++ base/auth.php
@@ -20,7 +20,7 @@ function ensure_player_stats(PDO $db, int $userId): void
     $stmt = $db->prepare(
         'INSERT IGNORE INTO player_stats
         (user_id, cash, respect, profession, energy, tickets, strength, endurance, intelligence, charisma, cunning, tickets_last_grant)
-        VALUES (:user_id, 0, 100, NULL, 100, 25, 10, 10, 10, 10, 10, NOW())'
+        VALUES (:user_id, 500, 100, NULL, 100, 25, 10, 10, 10, 10, 10, NOW())'
     );
     $stmt->execute(['user_id' => $userId]);
 }
