264d395a60e081fedc0023e26faeba0b15d0e31f
4wk7y6d2bw-cloud <4wk7y6d2bw@privaterelay.appleid.com>
2026-09-22 02:46:04 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?><!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MBZ</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <h1>siema</h1>
+</body>
+</html>
