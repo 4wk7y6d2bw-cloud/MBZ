3847463eb3484cc3a156323fe8b39f708051e1b4
4wk7y6d2bw-cloud <4wk7y6d2bw@privaterelay.appleid.com>
2026-09-22 05:16:27 +0200

    Use 500 dollars as initial player cash

--- install_stats.php
+++ install_stats.php
@@ -6,7 +6,7 @@ try {
     $pdo->exec("
         CREATE TABLE IF NOT EXISTS player_stats (
             user_id INT UNSIGNED NOT NULL PRIMARY KEY,
-            cash BIGINT UNSIGNED NOT NULL DEFAULT 0,
+            cash BIGINT UNSIGNED NOT NULL DEFAULT 500,
             respect INT UNSIGNED NOT NULL DEFAULT 100,
             profession VARCHAR(100) NULL DEFAULT NULL,
             energy TINYINT UNSIGNED NOT NULL DEFAULT 100,
@@ -26,7 +26,7 @@ try {
     $pdo->exec("
         INSERT IGNORE INTO player_stats
         (user_id, cash, respect, profession, energy, tickets, strength, endurance, intelligence, charisma, cunning, tickets_last_grant)
-        SELECT id, 0, 100, NULL, 100, 25, 10, 10, 10, 10, 10, NOW()
+        SELECT id, 500, 100, NULL, 100, 25, 10, 10, 10, 10, 10, NOW()
         FROM users
     ");
 
